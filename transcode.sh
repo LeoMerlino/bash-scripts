aa3b01c80186faa6adce7453a882641dfbf41995
Leo <lcm@keemail.me>
2026-07-18 14:42:03 +0100

    Add custom formats for audio transcode script

--- transcode.sh
+++ transcode.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-new_file="${1%.*}".opus
-[ "$new_file" != "$1" ] || { echo 'Already transcoded'; exit 1; }
-opusenc --bitrate 128 "$1" "$new_file" || ffmpeg -i "$1" -c:a libopus -b:a 128K "$new_file" || notify-send -u low -t 10000 -a 'Transcoding failed' 'Failed to transcode'
 
+transcode() {
+    new_file="${2%.*}"."$1"
+    ffmpeg -i "$2" "$new_file" || notify-send -u low -t 10000 -a 'Transcoding failed' 'Failed to transcode'
+}
 
+case "$1" in
+    opus)
+        new_file="${2%.*}".opus
+        [ "$new_file" != "$2" ] || { echo 'Already transcoded'; exit 1; }
+        opusenc --bitrate 128 "$2" "$new_file" || ffmpeg -i "$2" -c:a libopus -b:a 128K "$new_file" || notify-send -u low -t 10000 -a 'Transcoding failed' 'Failed to transcode'
+        ;;
+    prompt)
+        format=$(qarma --entry --text='Input extension (e.g., aiff, opus)')
+        printf '%s\0' "${@:2}" | xargs -0 -P"$(nproc)" -I{} "$0" "$format" "{}"
+        ;;
+    *)
+        new_file="${2%.*}"."$1"
+        ffmpeg -i "$2" "$new_file" || notify-send -u low -t 10000 -a 'Transcoding failed' 'Failed to transcode'
+        ;;
+esac
