328fa3370796aaab92b675562a5495ea49cc22fe
Leo <lcm@keemail.me>
2026-09-11 19:06:33 +0100

    Fix autoclicker logic

--- autoclicker.sh
+++ autoclicker.sh
@@ -2,11 +2,11 @@
 while true; do
 	if readlink -f /dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse; then
 		event=$(readlink -f /dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse)
-		test -e "$event" || {
+		if test -e "$event"; then
+			/home/leo/.cargo/bin/theclicker run -d"$event" -c25 -C0 -l188 -m189 -r187 -H --grab
+		else
 			echo "Mouse offline. Waiting..."
-			break
-		}
-		/home/leo/.cargo/bin/theclicker run -d"$event" -c25 -C0 -l188 -m189 -r187 -H --grab
+		fi
 	fi
 	sleep 5
 done
